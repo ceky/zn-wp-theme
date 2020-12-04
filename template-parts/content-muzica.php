a8a28363910959f07e14957522cbd78aee37b7f3
ceky <cekyy07@gmail.com>
2020-12-04 23:10:00 +0200

    Start styling muzica page

---
+++ template-parts/content-muzica.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package zilele-nordului
+ */
+
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php zilele_nordului_post_thumbnail(); ?>
+	<p class="title"><?php echo get_the_title(); ?></p>
+	<p class="date"><?php echo get_field( "date" ); ?></p>
+	<p class="location"><?php echo get_field( "location" ); ?></p>	
+
+</div><!-- #post-<?php the_ID(); ?> -->
